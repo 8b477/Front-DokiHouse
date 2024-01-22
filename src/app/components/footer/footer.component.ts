863a671b41d6635ae84e2599d1cc5e2579078eff
8b477 <jonathan_buchet@outlook.fr>
2024-01-22 22:00:39 +0100

    arrange SCSS for best responsive

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'footer',
+  selector: 'compo-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
--- src/app/pages/general/sign-up/sign-up.component.ts
+++ src/app/pages/general/sign-up/sign-up.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../components/footer/footer.component";
 
 @Component({
-  selector: 'app-sign-up',
-  standalone: true,
-  imports: [],
-  templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+    selector: 'app-sign-up',
+    standalone: true,
+    templateUrl: './sign-up.component.html',
+    styleUrl: './sign-up.component.scss',
+    imports: [FooterComponent]
 })
 export class SignUpComponent {
 
