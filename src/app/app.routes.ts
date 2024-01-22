ad8476084c12a3b075a60572c4cf7a48a941c259
8b477 <jonathan_buchet@outlook.fr>
2024-01-22 15:53:12 +0100

    A clean architecture for start & set up routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { SignUpComponent } from './pages/general/sign-up/sign-up.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/general/home/home.component';
+import { LoginComponent } from './pages/general/login/login.component';
+import { AboutComponent } from './pages/general/about/about.component';
+import { ContactComponent } from './pages/general/contact/contact.component';
+import { NotFoundComponent } from './pages/general/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    {'path' : '', component : HomeComponent},
+    {'path' : 'login', component : LoginComponent},
+    {'path' : 'signup', component : SignUpComponent},
+    {'path' : 'about', component : AboutComponent},
+    {'path' : 'contact', component : ContactComponent},
+    {'path' : '**', component : NotFoundComponent},
+];
---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'navigation',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+
+}
