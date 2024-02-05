bf5404b9c05d04a03ba307e43c3b52acf3f406ac
8b477 <jonathan_buchet@outlook.fr>
2024-02-05 23:36:44 +0100

    added lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,23 +6,32 @@ import { LoginComponent } from './pages/general/login/login.component';
 import { NotFoundComponent } from './pages/general/not-found/not-found.component';
 import { BlogComponent } from './pages/application/blog/blog.component';
 import { SupportComponent } from './pages/general/support/support.component';
+import { BonsaiComponent } from './pages/application/bonsai/bonsai.component';
+import { ProfilComponent } from './pages/application/profil/profil.component';
 
 
 export const routes: Routes = 
 [
     {'path' : '', component : HomeComponent},
-    {'path' : 'login', component : LoginComponent},
-    {'path' : 'signup', component : SignUpComponent},
+    {'path' : 'login', loadComponent : () => LoginComponent},
+    {'path' : 'signup', loadComponent : () => SignUpComponent},
 
 // --> SUPPORT
-    { 'path' : 'support', component : SupportComponent},
+    { 'path' : 'support', loadComponent : () => SupportComponent},
 
 // --> BLOG
-    { 'path' : 'blog', component : BlogComponent },
+    { 'path' : 'blog', loadComponent : () => BlogComponent },
+// ADD ---> https://apprendre.bonjour-angular.com/tips/guard-routes-with-can-match/
 
+
+    // ---> BONSAI
+    {'path' : 'bonsai', loadComponent : () => BonsaiComponent},
+
+    // --- PROFIL
+    {'path' : 'profil', loadComponent : () => ProfilComponent},
 
 // --> ERROR
-    {'path' : '**', component : NotFoundComponent},
+    {'path' : '**', loadComponent : () => NotFoundComponent},
 ];
 
 
