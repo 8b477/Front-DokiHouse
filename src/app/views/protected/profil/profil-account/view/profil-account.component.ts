60f0d7752f70ec25a5c4f34cbbc0e35e59697ad9
8b477 <jonathan_buchet@outlook.fr>
2024-03-07 20:05:46 +0100

    add a personalized toast to guide the user in the profile account options

--- src/app/views/protected/profil/profil-account/view/profil-account.component.ts
+++ src/app/views/protected/profil/profil-account/view/profil-account.component.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -12,12 +11,43 @@ import { Component, inject } from '@angular/core';
 export class ProfilAccountComponent {
 
   //VARIABLES
-  name : string = 'mark'
-  email : string = 'email@test.be'
-  role : string = 'larbin'
-  avatar : string = `https://api.dicebear.com/7.x/adventurer/svg?seed=${this.name}`
+  name : string = 'mark';
+  email : string = 'email@test.be';
+  role : string = 'larbin';
+  avatar : string = `https://api.dicebear.com/7.x/adventurer/svg?seed=${this.name}`;
+  passwd : string = '****';
 
 
-
+ activeFocusInput(): void{
+  document.getElementById('change-name')?.focus();
+ }
  
+
+
+  sendChangement()
+  {
+        const toastLiveExample  = document.getElementById('toast-wrapper')
+
+        if(toastLiveExample !== null)
+        {
+          toastLiveExample.style.display = 'block'
+          toastLiveExample.style.opacity = '1'
+        }
+  }
+
+closeToast(){
+  const toastLiveExample  = document.getElementById('toast-wrapper')
+
+        if(toastLiveExample !== null)
+        {
+          toastLiveExample.style.opacity = '0'
+          setTimeout(()=>{
+            toastLiveExample.style.display = 'none'
+          },500)
+
+        }
 }
+
+
+}
+
