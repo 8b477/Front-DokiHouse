390312266729129014dbd8eb0769f0e84570a1f4
8b477 <jonathan_buchet@outlook.fr>
2024-04-10 22:19:20 +0200

    build create bonsai option

--- src/app/views/protected/profil/profil-bonsai/component/create-bonsai/create-bonsai.component.ts
+++ src/app/views/protected/profil/profil-bonsai/component/create-bonsai/create-bonsai.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BonsaiServiceService } from '../../../../../../shared/services/bonsai-service/bonsai-service.service';
 import { BonsaiModel } from '../../../../../../API/models/bonsaiModels/bonsaiCreateModel';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-bonsai',
@@ -9,17 +10,45 @@ import { BonsaiModel } from '../../../../../../API/models/bonsaiModels/bonsaiCre
   templateUrl: './create-bonsai.component.html',
   styleUrl: './create-bonsai.component.scss'
 })
-export class CreateBonsaiComponent{
+export class CreateBonsaiComponent implements OnInit{
+  loginForm: any;
+  formBuilder: any;
 
   constructor(private bonsaiService : BonsaiServiceService){}
+  ngOnInit(): void {
+    this.buildFormAndValidator()
+  }
 
   newBonsai : BonsaiModel | undefined = undefined;
-  public createNewBonsai(bonsai : BonsaiModel){
 
+ private buildFormAndValidator() : void 
+  {
+    this.loginForm = this.formBuilder.group
+    ({ 
+        'title'   : ['', [Validators.required]],
+        'description' : ['', [Validators.required]]
+    });
+  }
+
+
+  private createNewBonsai(bonsai : BonsaiModel){
     this.bonsaiService.createBonsai(bonsai).subscribe(({
       next : (data) => { console.log(data); }
     }))
+  }
 
+  sendModel(){
+    const title  = this.loginForm.controls['title'].value
+    const description = this.loginForm.controls['description'].value
+
+    this.newBonsai = { name : title, description : description}
+
+      if(this.newBonsai.name == undefined || this.newBonsai.description == undefined)
+      {
+        console.error('userModel is not defined')
+      }
+  this.createNewBonsai(this.newBonsai);
   }
 
 }
+
